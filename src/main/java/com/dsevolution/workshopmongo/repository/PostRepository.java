b309ef2eecd8254dd7e24cd678f702f8de87e9ff
douglas souza <douglas.souza1997@hotmail.com>
2022-09-15 15:26:32 -0300

    Consulta simples com @Query

--- src/main/java/com/dsevolution/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dsevolution/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.dsevolution.workshopmongo.repository;
 import com.dsevolution.workshopmongo.entities.Post;
 import com.dsevolution.workshopmongo.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
